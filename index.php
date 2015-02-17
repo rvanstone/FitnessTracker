80145f0312cb7560f0fa9034edcb720b29dfef15
Rob Vanstone <rvanstone@xebialabs.com>
2015-02-17 09:56:50 +0000

    Updated to an Elastic Beanstalk application

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello World this is my first Elastic Beanstalk application</p>'; ?> 
  </body>
 </html>
