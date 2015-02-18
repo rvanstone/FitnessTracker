bb969b18cd7ab4d540d1389497d7ffc17efcffda
Rob Vanstone <rvanstone@xebialabs.com>
2015-02-18 11:48:47 +0000

    Added triggering functionality

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World this is my first modified Elastic Beanstalk application</p>'; ?> 
+ <?php echo '<p>Hello commit triggered  World this is my first modified Elastic Beanstalk application</p>'; ?> 
  </body>
 </html>
