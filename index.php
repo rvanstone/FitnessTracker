c04c884c7e1c8ee8cdf559627a270d83db84b191
Rob Vanstone <rvanstone@xebialabs.com>
2017-10-17 17:59:18 +0100

    An even weirder situation with some stuff in that shouldn't be/SAN-3

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Verification Test</title>
  </head>
  <body>
- <?php echo '<p> A Fine Hello commit triggered  World this is my first modified Elastic Beanstalk application</p>'; ?> 
+ <?php echo '<p> A Fine Hello commit triggered  World this is my first Elastic Beanstalk application</p>'; ?> 
  </body>
 </html>
